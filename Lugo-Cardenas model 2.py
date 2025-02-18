ca5f972c141303bc16ba26a4b316d59f4fac2c1a
Sayedessafi <150084985+Sayedessafi@users.noreply.github.com>
2025-02-18 16:18:47 +0000

    Update Lugo-Cardenas model 2.py

--- Lugo-Cardenas model 2.py
+++ Lugo-Cardenas model 2.py
@@ -17,7 +17,10 @@ I2 = m_pendulum * L2_pendulum**2
 
 
 control_mode =  ["FLC","NC","PD"]       #FLC(Feedback Linearization Control),NC(no control),PD(pd control)
-
+'''Kp_PD = 25 #this paramters shows a nice model ( better then what i was predicting,slightly better then FLC model which is kind weird  ) 
+Kd_PD = 2
+A_PD = -0.2 
+omega_PD = 1/3+np.pi'''
 # Control Parameters
 Kp_PD = 1.0  # Proportional gain for PD control
 Kd_PD = 0.05# Derivative gain for PD control
